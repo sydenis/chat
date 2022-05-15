e7837c1c4f19972d956e822bcb6111f300f77198
Lopez <you@example.com>
2022-05-15 19:02:51 +0300

    ready1

---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,39 @@
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun chatAdd() {
+    }
+
+    @Test
+    fun chatDel() {
+    }
+
+    @Test
+    fun chatsByOwner() {
+    }
+
+    @Test
+    fun chatUnreadCount() {
+    }
+
+    @Test
+    fun chatGetUnreadMessages() {
+    }
+
+    @Test
+    fun messageAdd() {
+    }
+
+    @Test
+    fun messageDel() {
+    }
+}
